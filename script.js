e2e1d147320539c59f155efcd0554256c000816f
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-10-26 00:44:28 +0100

    fixed font sizes

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ function playGame() {
         if (!winner && count === 9) {
           // console.log(playerWinner());
           winnerOverlay.classList.add("showwinner");
-          winners.textContent = "Draw Game";
+          winners.textContent = "Draw Game!";
         }
       }
     });
@@ -130,50 +130,50 @@ playGame();
 
 function playerWinner() {
   if (abc[0] === "X" && abc[1] === "X" && abc[2] === "X") {
-    return player1.value + " win";
+    return player1.value + " win!";
   } else if (abc[0] === "O" && abc[1] === "O" && abc[2] === "O") {
-    return player2.value + " win";
+    return player2.value + " win!";
   }
 
   if (def[0] === "X" && def[1] === "X" && def[2] === "X") {
-    return player1.value + " win";
+    return player1.value + " win!";
   } else if (def[0] === "O" && def[1] === "O" && def[2] === "O") {
-    return player2.value + " win";
+    return player2.value + " win!";
   }
 
   if (ghi[0] === "X" && ghi[1] === "X" && ghi[2] === "X") {
-    return player1.value + " win";
+    return player1.value + " win!";
   } else if (ghi[0] === "O" && ghi[1] === "O" && ghi[2] === "O") {
-    return player2.value + " win";
+    return player2.value + " win!";
   }
 
   if (adg[0] === "X" && adg[1] === "X" && adg[2] === "X") {
-    return player1.value + " win";
+    return player1.value + " win!";
   } else if (adg[0] === "O" && adg[1] === "O" && adg[2] === "O") {
-    return player2.value + " win";
+    return player2.value + " win!";
   }
 
   if (beh[0] === "X" && beh[1] === "X" && beh[2] === "X") {
-    return player1.value + " win";
+    return player1.value + " win!";
   } else if (beh[0] === "O" && beh[1] === "O" && beh[2] === "O") {
-    return player2.value + " win";
+    return player2.value + " win!";
   }
 
   if (cfi[0] === "X" && cfi[1] === "X" && cfi[2] === "X") {
-    return player1.value + " win";
+    return player1.value + " win!";
   } else if (cfi[0] === "O" && cfi[1] === "O" && cfi[2] === "O") {
-    return player2.value + " win";
+    return player2.value + " win!";
   }
 
   if (aei[0] === "X" && aei[1] === "X" && aei[2] === "X") {
-    return player1.value + " win";
+    return player1.value + " win!";
   } else if (aei[0] === "O" && aei[1] === "O" && aei[2] === "O") {
-    return player2.value + " win";
+    return player2.value + " win!";
   }
 
   if (ceg[0] === "X" && ceg[1] === "X" && ceg[2] === "X") {
-    return player1.value + " win";
+    return player1.value + " win!";
   } else if (ceg[0] === "O" && ceg[1] === "O" && ceg[2] === "O") {
-    return player2.value + " win";
+    return player2.value + " win!";
   }
 }
